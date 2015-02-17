3f5b3f290ea268ca591d853bcf123ae2dc8f99a3
Nikita Marchenko <nmarchenko@mirantis.com>
2015-02-17 21:44:23 +0200

    Base flask app

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pimai0oh',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
