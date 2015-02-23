e418a18bcf5c23b49b65157222eb6a1530ff86df
Nikita Marchenko <nmarchenko@mirantis.com>
2015-02-23 16:25:25 +0200

    fix missed data files

--- setup.py
+++ setup.py
@@ -6,13 +6,22 @@ from pip.req import parse_requirements
 install_requires_pip = parse_requirements('requirements.txt',
                                           session=uuid.uuid1())
 
+
+install_requires_setuptools = []
+
+for ir in install_requires_pip:
+    install_requires_setuptools.append(str(ir.req))
+
 setup(
     name='pimai0oh',
     version='',
-    packages=[''],
+    packages=find_packages(),
     url='',
     license='',
     author='',
     author_email='',
-    description=''
+    description='',
+    install_requires=install_requires_setuptools,
+    setup_requires=["setuptools_git >= 0.3", ],
+    include_package_data=True
 )
